e261dfcc06d330c544cca98d9c0563ac86c6f9d5
Simatwa <smartwacaleb@gmail.com>
2023-12-18 23:33:22 +0300

    Fix static files rendering issue  - Drop & untrack venv  - Make mainpage as landing page  - Migrate db to sqlite3  - Add independencies - requirements.txt  - Format code with `black`  - Add page logo  - Add .gitignore file  - Untrack farmer.db

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,15 +1,24 @@
 from django.urls import path
-from .views import (Mainpage,Show_employees,Add_employees,Delete_employees,Update_employees,Show_crops,Add_crops,Update_crops)
+from .views import (
+    Mainpage,
+    Show_employees,
+    Add_employees,
+    Delete_employees,
+    Update_employees,
+    Show_crops,
+    Add_crops,
+    Update_crops,
+)
 
-app_name='homepage'
+app_name = "homepage"
 
-urlpatterns =[
-    path('mainpage/', Mainpage, name='mainpage'),
-    path('show_employees/', Show_employees, name='show-employees'),
-    path('add_employees/', Add_employees, name='add-employees'),
-    path('delete_employees/<int:Eid>/', Delete_employees, name='delete-employees'),
-    path('update_employees/<int:Eid>/', Update_employees, name='update-employees'),
-    path('show_crops/', Show_crops, name='show-crops'),
-    path('add-crops/', Add_crops, name= 'add-crops'),
-    path('update_crops/<int:Cid>/', Update_crops, name='update-crops'),
+urlpatterns = [
+    path("", Mainpage, name="mainpage"),
+    path("show_employees/", Show_employees, name="show-employees"),
+    path("add_employees/", Add_employees, name="add-employees"),
+    path("delete_employees/<int:Eid>/", Delete_employees, name="delete-employees"),
+    path("update_employees/<int:Eid>/", Update_employees, name="update-employees"),
+    path("show_crops/", Show_crops, name="show-crops"),
+    path("add-crops/", Add_crops, name="add-crops"),
+    path("update_crops/<int:Cid>/", Update_crops, name="update-crops"),
 ]
